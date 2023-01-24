998f83722eb9bc612128c2b68a8b5b27b32d7ee9
Viktoriya Lyabakh <vikysia1985@gmail.com>
2023-01-24 00:08:51 -0600

    add utilities

---
+++ src/test/java/utilities/ConfigurationReader.java
@@ -0,0 +1,22 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+        FileInputStream fileInputStream = new FileInputStream("configuration.properties");
+            properties.load(fileInputStream);
+            fileInputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getProperty(String key){
+      return properties.getProperty(key);
+    }
+}
